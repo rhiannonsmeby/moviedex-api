e234dbfa2d7832bb52630da284be393c82229c48
Rhiannon Smeby <smebyrhi@gmail.com>
2020-12-31 18:00:04 -0600

    added cors and helmet

--- server.js
+++ server.js
@@ -2,11 +2,16 @@
 require('dotenv').config();
 const express = require('express');
 const morgan = require('morgan');
+const cors = require('cors');
+const helmet = require('helmet');
 const MOVIEDEX = require('./moviedex.json');
 
-console.log(process.env.API_TOKEN);
-
 const app = express();
+
+
+app.use(morgan('dev'));
+app.use(helmet());
+app.use(cors());
 
 app.use(function validateBearerToken(req, res, next) {
   const apiToken = process.env.API_TOKEN;
@@ -20,7 +25,6 @@ app.use(function validateBearerToken(req, res, next) {
   next();
 });
 
-app.use(morgan('dev'));
 
 function handleGetMovies(req, res) {
   let response = MOVIEDEX;
