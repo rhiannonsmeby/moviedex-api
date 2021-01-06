76b28363fb3f924659f651a8161545ff6a036c6b
Rhiannon Smeby <smebyrhi@gmail.com>
2021-01-06 11:05:10 -0600

    implemented production changes

--- server.js
+++ server.js
@@ -8,16 +8,14 @@ const MOVIEDEX = require('./moviedex.json');
 
 const app = express();
 
-
-app.use(morgan('dev'));
+const morganSetting = process.env.NODE_ENV === 'production' ? 'tiny' : 'common';
+app.use(morgan(morganSetting));
 app.use(helmet());
 app.use(cors());
 
 app.use(function validateBearerToken(req, res, next) {
   const apiToken = process.env.API_TOKEN;
   const authToken = req.get('Authorization');
-
-  console.log('validate bearer token middleware');
 
   if(!authToken || authToken.split(' ')[1] !== apiToken) {
     return res.status(401).json({error: 'Unauthorized request'});
@@ -48,7 +46,18 @@ function handleGetMovies(req, res) {
 
 app.get('/movie', handleGetMovies);
 
-const PORT = 8050;
+app.use((error, req, res, next) => {
+  let response;
+  if (process.env.NODE_ENV === 'production') {
+    response = {error: {message: 'server error'}};
+  } else {
+    response = {error};
+  }
+  res.status(500).json(response);
+  next();
+});
+
+const PORT = process.env.PORT || 8050;
 app.listen(PORT, () => {
   console.log(`Server listening at http://localhost:${PORT}`);
 });
